fbf186f417d41e852281260f328731c1f84e81d2
Adam Schubert <adam.schubert@sg1-game.net>
2018-06-05 22:10:24 +0200

    Get version string from version class

--- version/Version.py
+++ version/Version.py
@@ -401,7 +401,7 @@ class Version(object):
                 return
         modified_files = self.mark_version_files(set_version, dry=self._options['--dry'])
         self.log.debug('Modified files: {}'.format(modified_files))
-        print('{} files has been modified to contain version string {}'.format(len(modified_files), self._options['<version>']))
+        print('{} files has been modified to contain version string {}'.format(len(modified_files), set_version))
 
         repo = Repo(self._project_dir)
 
