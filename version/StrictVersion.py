4dcb85dbea6d954a2da567335ad7544ccd3e7fd9
Adam Schubert <adam.schubert@sg1-game.net>
2024-09-22 23:26:09 +0200

    fix(StrictVersion): Override _key too

--- version/StrictVersion.py
+++ version/StrictVersion.py
@@ -1,5 +1,5 @@
 from typing import Tuple
-from packaging.version import Version, _Version
+from packaging.version import Version, _Version, _cmpkey
 
 
 class StrictVersion(Version):
@@ -7,6 +7,7 @@ class StrictVersion(Version):
     def _new_from_release(self, release: Tuple[int, int, int]) -> 'StrictVersion':
         new_version = StrictVersion(str(self))
 
+        # Override internal version NamedTuple
         new_version._version = _Version(
             epoch=self._version.epoch,
             release=release,
@@ -14,6 +15,16 @@ class StrictVersion(Version):
             post=self._version.post,
             dev=self._version.dev,
             local=self._version.local,
+        )
+
+        # Override internal key
+        new_version._key = _cmpkey(
+            new_version._version.epoch,
+            new_version._version.release,
+            new_version._version.pre,
+            new_version._version.post,
+            new_version._version.dev,
+            new_version._version.local,
         )
 
         return new_version
